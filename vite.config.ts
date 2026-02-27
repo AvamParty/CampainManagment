52e4acc6810ca17311bcf502101638a24da83009
Taha <mtr1681@gmail.com>
2026-02-27 23:55:47 +0330

    fix: add API_BASE_URL to env

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 const srcPath = decodeURIComponent(new URL('./src', import.meta.url).pathname)
 
-export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': srcPath,
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
+  return {
+    plugins: [
+      // The React and Tailwind plugins are both required for Make, even if
+      // Tailwind is not being actively used – do not remove them
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        // Alias @ to the src directory
+        '@': srcPath,
+      },
+    },
+
+    server: {
+      host: env.VITE_DEV_HOST || '127.0.0.1',
+      port: Number(env.VITE_DEV_PORT) || 3001,
+    },
+
+    // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+    assetsInclude: ['**/*.svg', '**/*.csv'],
+  }
 })
